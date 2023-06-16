d7fdf42fecb3185f05faedff5781144340f9053b
hideo araki <bugcreator@bugcreators-cafe.com>
2023-06-17 01:11:05 +0900

    connect to db.

--- server.js
+++ server.js
@@ -4,6 +4,17 @@ const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
 const PORT = 3000;
+const mongoose = require("mongoose");
+require("dotenv").config();
+// connect to db
+mongoose
+  .connect(process.env.MONGOURL)
+  .then(() => {
+    console.log("connect done...");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 // ミドルウェア
 app.use("/api/users", userRoute);
