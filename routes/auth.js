eeaffcde3f7d4808b7e84b9fdc17653ee2a4732d
hideo araki <bugcreator@bugcreators-cafe.com>
2023-06-17 01:52:14 +0900

    create user register end point.

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,23 @@
 const router = require("express").Router();
+const User = require("../models/User");
 
-router.get("/", (req, res) => {
-  res.send("auth router");
+// ユーザ登録
+router.post("/register", async (req, res) => {
+  try {
+    const newUser = await new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
+    const user = await newUser.save();
+    return res.status(200).json(user);
+  } catch (error) {
+    return req.statusCode(500).json(err);
+  }
 });
+
+// router.get("/", (req, res) => {
+//   res.send("auth router");
+// });
 
 module.exports = router;
